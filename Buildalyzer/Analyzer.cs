f3ee45dc4cf2db2579475b8fa689a27c1267fbfd
Dave Glick <dave@daveaglick.com>
2017-09-22 16:19:25 -0400

    Commit before removing the targets/copy MSBuild runtime approach

---
+++ Buildalyzer/Analyzer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Microsoft.Build.Evaluation;
+using Microsoft.Build.Utilities;
+
+namespace Buildalyzer
+{
+    public class Analyzer
+    {
+        public Project Project { get; private set; }
+
+        public Analyzer(string fileName)
+        {
+            ProjectCollection projectCollection = new ProjectCollection();
+            projectCollection.AddToolset(new Toolset(ToolLocationHelper.CurrentToolsVersion, AppContext.BaseDirectory, projectCollection, string.Empty));
+            Project = projectCollection.LoadProject(fileName);
+        }
+    }
+}

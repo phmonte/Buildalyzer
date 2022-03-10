06084dec9bf10b1310faead7140d5b70adba29f8
Zhang <mingxzh@amazon.com>
2022-03-10 15:40:14 -0800

    Address PR comment to cover case sensitive scenario and added test coverage as well.

---
+++ tests/projects/DuplicateProjectReferences/MainProject/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DependentProject;
+
+namespace MainProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LibInterface lib = new LibInterface();
+            lib.Execute();
+
+            return;
+        }
+    }
+}

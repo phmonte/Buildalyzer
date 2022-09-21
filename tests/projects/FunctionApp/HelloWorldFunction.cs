cb0dd28f10c365799f5498387210dd884563a2eb
Dave Glick <dave@daveaglick.com>
2022-09-21 17:10:55 -0400

    Added failing tests for Functions apps but couldn't get them to work (https://github.com/daveaglick/Buildalyzer/issues/210)

---
+++ tests/projects/FunctionApp/HelloWorldFunction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+namespace FunctionApp;
+
+public class HelloWorldFunction
+{
+    [FunctionName("HelloWorld")]
+    public static async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]
+        HttpRequest req,
+        ILogger log)
+    {
+        log.LogInformation("Hello Buildalyzer!");
+        return new OkObjectResult("Hello World!");
+    }
+}

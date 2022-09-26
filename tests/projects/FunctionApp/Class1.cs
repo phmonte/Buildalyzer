c3df4bea7e506905cf1f1fa5e0c8dc4aa7e8fa03
Bonner Earle <Bonner.Earle@288group.com>
2022-09-26 13:37:58 +0100

    Adding project to solution and missing expected source file

---
+++ tests/projects/FunctionApp/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionApp
+{
+    internal class Class1
+    {
+    }
+}

eddd90965abb9653cb6abae2562f269474ccede7
Corniel Nobel <corniel@gmail.com>
2024-01-28 19:27:55 +0100

    Introduction of CompilerItems and CompilerProperties and CompilerItems(Collection).

---
+++ src/Buildalyzer/Compiler/CompilerItemsCollection.cs
@@ -0,0 +1,62 @@
+﻿#nullable enable
+
+using Microsoft.Build.Framework;
+
+namespace Buildalyzer;
+
+[DebuggerDisplay("Count = {Count}")]
+[DebuggerTypeProxy(typeof(Diagnostics.CollectionDebugView<CompilerItems>))]
+public sealed class CompilerItemsCollection : IReadOnlyCollection<CompilerItems>
+{
+    [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+    private readonly Dictionary<string, IReadOnlyCollection<ITaskItem>> _values = new Dictionary<string, IReadOnlyCollection<ITaskItem>>(StringComparer.OrdinalIgnoreCase);
+
+    private CompilerItemsCollection()
+    {
+    }
+
+    public CompilerItemsCollection(IEnumerable<KeyValuePair<string, IReadOnlyCollection<ITaskItem>>> values)
+    {
+        _values = values.ToDictionary(kvp => kvp.Key, kvp => kvp.Value, StringComparer.OrdinalIgnoreCase);
+    }
+
+    public int Count => _values.Count;
+
+    [Pure]
+    public CompilerItems? TryGet(string key)
+        => _values.TryGetValue(key, out IReadOnlyCollection<ITaskItem>? values)
+            ? new CompilerItems(key, values)
+            : null;
+
+    [Pure]
+    public IEnumerator<CompilerItems> GetEnumerator()
+    {
+        return Select().GetEnumerator();
+
+        IEnumerable<CompilerItems> Select() => _values.Select(kvp => new CompilerItems(kvp.Key, kvp.Value));
+    }
+
+    [Pure]
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    [Pure]
+    internal static CompilerItemsCollection FromDictionaryEntries(IEnumerable properties)
+    {
+        CompilerItemsCollection props = new CompilerItemsCollection();
+
+        foreach (DictionaryEntry entry in properties.ToDictionaryEntries())
+        {
+            if (entry.Key?.ToString() is { Length: > 0 } key && entry.Value is ITaskItem task)
+            {
+                if (!props._values.TryGetValue(key, out IReadOnlyCollection<ITaskItem>? values)
+                    || values is not List<ITaskItem> editable)
+                {
+                    editable = new List<ITaskItem>();
+                    props._values[key] = editable;
+                }
+                editable.Add(task);
+            }
+        }
+        return props;
+    }
+}
---
+++ src/Buildalyzer/Compiler/CompilerProperties.cs
@@ -0,0 +1,58 @@
+﻿#nullable enable
+
+namespace Buildalyzer;
+
+[DebuggerDisplay("Count = {Count}")]
+[DebuggerTypeProxy(typeof(Diagnostics.CollectionDebugView<CompilerProperty>))]
+#pragma warning disable CA1710 // Identifiers should have correct suffix
+
+// CompilerProperties describes the type the best.
+public sealed class CompilerProperties : IReadOnlyCollection<CompilerProperty>
+#pragma warning restore CA1710 // Identifiers should have correct suffix
+{
+    [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+    private readonly Dictionary<string, object> _values = new Dictionary<string, object>(StringComparer.OrdinalIgnoreCase);
+
+    private CompilerProperties()
+    {
+    }
+
+    public CompilerProperties(IEnumerable<KeyValuePair<string, object>> values)
+    {
+        _values = values.ToDictionary(kvp => kvp.Key, kvp => kvp.Value, StringComparer.OrdinalIgnoreCase);
+    }
+
+    public int Count => _values.Count;
+
+    [Pure]
+    public CompilerProperty? TryGet(string key)
+        => _values.TryGetValue(key, out object? value)
+            ? new CompilerProperty(key, value)
+            : null;
+
+    [Pure]
+    public IEnumerator<CompilerProperty> GetEnumerator()
+    {
+        return Select().GetEnumerator();
+
+        IEnumerable<CompilerProperty> Select() => _values.Select(kvp => new CompilerProperty(kvp.Key, kvp.Value));
+    }
+
+    [Pure]
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    [Pure]
+    internal static CompilerProperties FromDictionaryEntries(IEnumerable properties)
+    {
+        CompilerProperties props = new CompilerProperties();
+
+        foreach (DictionaryEntry entry in properties.ToDictionaryEntries())
+        {
+            if (entry.Key?.ToString() is { Length: > 0 } key && entry.Value is { })
+            {
+                props._values.Add(key, entry.Value);
+            }
+        }
+        return props;
+    }
+}

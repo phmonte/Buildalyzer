ab351e8fa3ca4a8b58ff3b7933a4ffa2857906b8
Corniel Nobel <corniel@gmail.com>
2024-02-23 20:05:50 +0100

    Define shared base.

--- src/Buildalyzer/Compiler/CSharpCompilerCommand.cs
+++ src/Buildalyzer/Compiler/CSharpCompilerCommand.cs
@@ -1,32 +1,11 @@
-﻿using System.Collections.Immutable;
-using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
+﻿using Microsoft.CodeAnalysis.CSharp;
 
 namespace Buildalyzer;
 
-public sealed record CSharpCompilerCommand : CompilerCommand
+public sealed record CSharpCompilerCommand : RoslynBasedCompilerCommand<CSharpCommandLineArguments>
 {
-    public CSharpCompilerCommand(CSharpCommandLineArguments arguments)
+    public CSharpCompilerCommand(CSharpCommandLineArguments arguments) 
+        : base(arguments)
     {
-        Arguments = Guard.NotNull(arguments);
-        PreprocessorSymbolNames = Arguments.ParseOptions.PreprocessorSymbolNames.ToImmutableArray();
     }
-
-    public CSharpCommandLineArguments Arguments { get; }
-
-    public override CompilerLanguage Language => CompilerLanguage.CSharp;
-
-    public override ImmutableArray<Diagnostic> Errors => Arguments.Errors;
-
-    public override ImmutableArray<CommandLineSourceFile> SourceFiles => Arguments.SourceFiles;
-
-    public override ImmutableArray<CommandLineSourceFile> AdditionalFiles => Arguments.AdditionalFiles;
-
-    public override ImmutableArray<CommandLineSourceFile> EmbeddedFiles => Arguments.EmbeddedFiles;
-
-    public override ImmutableArray<CommandLineAnalyzerReference> AnalyzerReferences => Arguments.AnalyzerReferences;
-
-    public override ImmutableArray<string> PreprocessorSymbolNames { get; }
-
-    public override ImmutableArray<string> ReferencePaths => Arguments.ReferencePaths;
 }
---
+++ src/Buildalyzer/Compiler/RoslynBasedCompilerCommand.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+
+namespace Buildalyzer;
+
+public abstract record RoslynBasedCompilerCommand<TArguments> : CompilerCommand
+    where TArguments : CommandLineArguments
+{
+    protected RoslynBasedCompilerCommand(TArguments arguments)
+    {
+        Arguments = Guard.NotNull(arguments);
+        PreprocessorSymbolNames = Arguments.ParseOptions.PreprocessorSymbolNames.ToImmutableArray();
+    }
+
+    public TArguments Arguments { get; }
+
+    public override CompilerLanguage Language => CompilerLanguage.VisualBasic;
+
+    public override ImmutableArray<Diagnostic> Errors => Arguments.Errors;
+
+    public override ImmutableArray<CommandLineSourceFile> SourceFiles => Arguments.SourceFiles;
+
+    public override ImmutableArray<CommandLineSourceFile> AdditionalFiles => Arguments.AdditionalFiles;
+
+    public override ImmutableArray<CommandLineSourceFile> EmbeddedFiles => Arguments.EmbeddedFiles;
+
+    public override ImmutableArray<CommandLineAnalyzerReference> AnalyzerReferences => Arguments.AnalyzerReferences;
+
+    public override ImmutableArray<string> PreprocessorSymbolNames { get; }
+
+    public override ImmutableArray<string> ReferencePaths => Arguments.ReferencePaths;
+}

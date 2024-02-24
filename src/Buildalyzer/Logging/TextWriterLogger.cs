ec7fd3e8f1e7a49507de6ae51d2ae609060d860e
Corniel Nobel <corniel@gmail.com>
2024-02-24 09:15:24 +0100

    Providing `EmptyDisposable` as part of the public API should not be part of Buildalyzer.

--- src/Buildalyzer/Logging/TextWriterLogger.cs
+++ src/Buildalyzer/Logging/TextWriterLogger.cs
@@ -4,19 +4,26 @@ using Microsoft.Extensions.Logging;
 
 namespace Buildalyzer.Logging;
 
-internal class TextWriterLogger : ILogger
+/// <summary>Implements <see cref="ILogger"/> using a <see cref="TextWriter"/>.</summary>
+internal sealed class TextWriterLogger(TextWriter textWriter) : ILogger
 {
-    private readonly TextWriter _textWriter;
+    private readonly TextWriter _textWriter = textWriter;
 
-    public TextWriterLogger(TextWriter textWriter)
-    {
-        _textWriter = textWriter;
-    }
-
+    /// <inheritdoc />
     public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter) =>
         _textWriter.Write(formatter(state, exception));
 
+    /// <inheritdoc />
     public bool IsEnabled(LogLevel logLevel) => true;
 
-    public IDisposable BeginScope<TState>(TState state) => new EmptyDisposable();
+    /// <inheritdoc />
+    public IDisposable BeginScope<TState>(TState state) => new Scope();
+
+    private sealed class Scope : IDisposable
+    {
+        public void Dispose()
+        {
+            // Nothing to dispose.
+        }
+    }
 }

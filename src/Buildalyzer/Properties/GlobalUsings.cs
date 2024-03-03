e7f03dd69f665977b057c6f305919651b9b15c19
Corniel Nobel <corniel@gmail.com>
2024-03-03 04:30:13 +0100

    Define a lightweight struct to represent paths (#257)

--- src/Buildalyzer/Properties/GlobalUsings.cs
+++ src/Buildalyzer/Properties/GlobalUsings.cs
@@ -1,6 +1,8 @@
 ﻿global using System;
 global using System.Collections.Generic;
 global using System.Collections.Immutable;
+global using System.Diagnostics;
+global using System.Diagnostics.CodeAnalysis;
 global using System.Diagnostics.Contracts;
 global using System.Linq;
 global using System.Text;
---
+++ tests/Buildalyzer.Tests/IO/IOPathFixture.cs
@@ -0,0 +1,21 @@
+﻿using Buildalyzer.IO;
+using FluentAssertions;
+
+namespace Buildalyzer.Tests.IO;
+
+public class IOPathFixture
+{
+#if Is_Windows
+    [Test]
+    public void Is_case_insensitive_on_windows()
+        => IOPath.IsCaseSensitive.Should().BeFalse();
+#endif
+
+    [Test]
+    public void is_seperator_agnostic()
+        => IOPath.Parse(".\\root\\test\\somefile.txt").Should().Be(IOPath.Parse("./root/test/somefile.txt"));
+
+    [TestCase(@"c:\Program Files\Buildalyzer")]
+    public void supports_type_conversion(IOPath path)
+        => path.Should().Be(IOPath.Parse(@"c:\Program Files\Buildalyzer"));
+}

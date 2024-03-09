b6d7aae6c2f28d5abeb46b87dff59fdfea36f2d2
Corniel Nobel <corniel@gmail.com>
2024-03-09 22:16:37 +0100

    Create a test context to take away a lot of the repeating plumbing code.

---
+++ test/Buildalyzer.TestTools/Context.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+
+namespace Buildalyzer.TestTools;
+
+public static class Context
+{
+    [Pure]
+    public static BuildalyzerTestContext ForProject(string path) => new(GetProjectPath(path));
+
+    private static FileInfo GetProjectPath(string file)
+    {
+        var location = new FileInfo(typeof(Context).Assembly.Location).Directory!;
+        return new FileInfo(Path.Combine(
+            location.FullName,
+            "..",
+            "..",
+            "..",
+            "..",
+            "projects",
+            file));
+    }
+}

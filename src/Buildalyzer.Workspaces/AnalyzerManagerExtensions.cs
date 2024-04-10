1f379d39b7b1a7104e31bd26f9e73b8bba2928df
Corniel Nobel <corniel@gmail.com>
2024-04-10 11:59:06 +0200

    Logging should not use interpolated strings.

--- src/Buildalyzer.Workspaces/AnalyzerManagerExtensions.cs
+++ src/Buildalyzer.Workspaces/AnalyzerManagerExtensions.cs
@@ -16,8 +16,8 @@ public static class AnalyzerManagerExtensions
     {
         ILogger logger = manager.LoggerFactory?.CreateLogger<AdhocWorkspace>();
         AdhocWorkspace workspace = new AdhocWorkspace();
-        workspace.WorkspaceChanged += (sender, args) => logger?.LogDebug($"Workspace changed: {args.Kind.ToString()}{System.Environment.NewLine}");
-        workspace.WorkspaceFailed += (sender, args) => logger?.LogError($"Workspace failed: {args.Diagnostic}{System.Environment.NewLine}");
+        workspace.WorkspaceChanged += (sender, args) => logger?.LogDebug("Workspace changed: {Kind}{NewLine}", args.Kind, System.Environment.NewLine);
+        workspace.WorkspaceFailed += (sender, args) => logger?.LogError("Workspace failed: {Diagnostic}{NewLine}", args.Diagnostic, System.Environment.NewLine);
         return workspace;
     }
 

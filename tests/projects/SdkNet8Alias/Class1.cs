01038f3477559cc3fdf504aaa5b4973267d807f8
Cyrille DUPUYDAUBY <dupdob@gmail.com>
2024-11-12 16:15:40 +0100

    fix: test errors

--- tests/projects/SdkNet8Alias/Class1.cs
+++ tests/projects/SdkNet8Alias/Class1.cs
@@ -1,17 +1,15 @@
 ﻿extern alias Alias;
-extern alias OtherAlias;
 
 using Alias::Xunit;
-using OtherAlias::SdkNet8CS12FeaturesProject;
 
 namespace SdkNet8Alias
 {
-    public class UnitTest1
+    public class Class1
     {
         [Fact]
         public void Test1()
         {
-            var t = new Class1();
+
         }
     }
 }
